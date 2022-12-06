bb66a18e35deeece2be906068d80a78f5a6eb78c
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-12-06 11:13:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
 
-bool maiuscola (char &k){
-    if (k>64 && k<91){
-        k+=32;
-        return true; 
-    } else {
-        return false;  
-    }
-    }
-
-
-bool minuscola (char &k){
-    if (k>96 && k<123){
-        k-=32;
-        return true;  
-    } else {
-        return false;  
-    }
-    }
 
 int main() {
     char a;
     cin >>a;
-    if (maiuscola (a) || minuscola (a)){
-        cout<<"vero"<<endl;
+    if (maiuscola (a)==true || minuscola (a)==true){
+        cout<< "vero" <<endl;
         cout<<a;
     } else {
         cout <<"falso";
