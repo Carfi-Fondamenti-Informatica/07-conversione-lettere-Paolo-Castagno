15e41c13f250bccdf81324a803f26526afe11df3
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-12-06 10:56:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,22 +1,2 @@
-#include "lib.h"
 
-bool maiuscola (char &k){
-    if (k>64 && k<91){
-        k+=32;
-        return true; 
-    } else {
-        return false;  
-    }
-    }
-
-
-bool minuscola (char &k){
-    if (k>96 && k<123){
-        k-=32;
-        return true;  
-    } else {
-        return false;  
-    }
-    
-    }
 
